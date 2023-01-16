7f1a172784a2f48dca1881619f7ea9e3a9dbd62c
Leroy Korterink <leroy@mediamonks.com>
2023-01-16 16:17:18 +0100

    Lint selector and id name in `@mediamonks/stylelint-config-scss-muban`

--- packages/stylelint-config-scss-muban/index.js
+++ packages/stylelint-config-scss-muban/index.js
@@ -1,10 +1,28 @@
+// eslint-disable-next-line prefer-named-capture-group, unicorn/no-unsafe-regex
+const kebabCase = /^([a-z][\da-z]*)(-[\da-z]+)*$/u;
+const patternRuleOptions = [
+  kebabCase,
+  {
+    message(selector) {
+      return `Expected ${selector} to be kebab-case`;
+    },
+  },
+];
+
 // eslint-disable-next-line unicorn/prefer-module
 module.exports = {
   /**
    * Overrides to make @mediamonks/stylelint-config-scss compatible with muban projects
    */
   rules: {
-    'max-nesting-depth': null,
-    'selector-class-pattern': null,
+    'max-nesting-depth': [
+      // eslint-disable-next-line no-magic-numbers
+      4,
+      {
+        ignore: ['blockless-at-rules'],
+      },
+    ],
+    'selector-class-pattern': patternRuleOptions,
+    'selector-id-pattern': patternRuleOptions,
   },
 };
